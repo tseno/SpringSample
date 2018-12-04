87558c271c3d24f7c80a647c4180d9053cec8660
tseno <tseno26@gmail.com>
2018-12-04 23:12:29 +0900

    validate追加

--- src/main/java/com/example/springsample/login/controller/SignupController.java
+++ src/main/java/com/example/springsample/login/controller/SignupController.java
@@ -4,6 +4,7 @@ import com.example.springsample.trySpring.SignupForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,7 +32,7 @@ public class SignupController {
     }
 
     @PostMapping("/signup")
-    public String postSignup(@ModelAttribute SignupForm form, BindingResult bindingResult, Model model) {
+    public String postSignup(@ModelAttribute @Validated SignupForm form, BindingResult bindingResult, Model model) {
 
         if (bindingResult.hasErrors()) {
             return getSignup(form, model);
--- src/main/java/com/example/springsample/trySpring/SignupForm.java
+++ src/main/java/com/example/springsample/trySpring/SignupForm.java
@@ -1,19 +1,35 @@
 package com.example.springsample.trySpring;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Data
 public class SignupForm {
+
+    @NotBlank(message = "{require_check}")
+    @Email(message = "{email_check}")
     private String userId;
+
+    @NotBlank(message = "{require_check}")
+    @Length(min = 4, max = 100, message = "{length_check}")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "{pattern_check}")
     private String password;
+
+    @NotBlank(message = "{require_check}")
     private String userName;
 
+    @NotNull(message = "{require_check}")
     @DateTimeFormat(pattern = "yyyy/MM/dd")
     private Date birthday;
 
+    @Min(value = 20, message = "{min_check}")
+    @Max(value = 100, message = "{max_check}")
     private int age;
+
+    @AssertFalse(message = "{false_check}")
     private boolean marriage;
 }
